c5d4c5ede8b5ec403a8bf89555bcf98520eeaa22
adrianbielsa1 <50265158+adrianbielsa1@users.noreply.github.com>
2019-05-20 11:41:47 -0300

    Added and defined methods reciprocal(), is_zero(), is_real() and is_pure()

--- algebra/Complex.py
+++ algebra/Complex.py
@@ -45,3 +45,21 @@ class Complex:
     
     def conjugate(self):
         return Complex(self.real(), -self.imaginary())
+        
+    def reciprocal(self):
+        if self.is_zero():
+            raise ValueError("Complex is zero")
+            
+        sum_of_squares = self.real() ** 2 + self.imaginary() ** 2
+        
+        return Complex(self.real() / sum_of_squares, -self.imaginary() / sum_of_squares)
+            
+    def is_zero(self):
+        return math.isclose(self.real() ** 2 + self.imaginary() ** 2, 0)
+            
+    def is_real(self):
+        return math.isclose(self.imaginary(), 0)
+        
+    def is_pure(self):
+        return math.isclose(self.real(), 0)
+
