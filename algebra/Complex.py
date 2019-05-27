a0be7254e56adc389b055e069355b2ec98c02bb5
adrianbielsa1 <50265158+adrianbielsa1@users.noreply.github.com>
2019-05-27 13:09:25 -0300

    Removed dangling trailing spaces

--- algebra/Complex.py
+++ algebra/Complex.py
@@ -2,76 +2,76 @@ import math
 
 
 class Complex:
-    
+
     def __init__(self, re: float, im: float):
         self.__re = re
         self.__im = im
-        
+
     def __add__(self, other):
         real = self.real() + other.real()
         imaginary = self.imaginary() + other.imaginary()
-        
+
         return Complex(real, imaginary)
-        
+
     def __sub__(self, other):
         real = self.real() - other.real()
         imaginary = self.imaginary() - other.imaginary()
-        
+
         return Complex(real, imaginary)
-        
+
     def __mul__(self, other: float):
         return self @ Complex(other, 0)
-        
+
     def __rmul__(self, other: float):
         return self * other
-        
+
     def __matmul__(self, other):
         real = self.real() * other.real() - self.imaginary() * other.imaginary()
         imaginary = self.real() * other.imaginary() + self.imaginary() * other.real()
-        
+
         return Complex(real, imaginary)
-        
+
     def __eq__(self, other) -> bool:
         if not math.isclose(self.real(), other.real()):
             return False
-            
+
         if not math.isclose(self.imaginary(), other.imaginary()):
             return False
-            
+
         return True
-        
+
     def __str__(self):
         return str(self.__re) + "+" + str(self.__im) + "i"
-        
+
     def __repr__(self):
         return "Complex(" + str(self.__re) + ", " + str(self.__im) + ")"
-        
+
     def real(self) -> float:
         return self.__re
-    
+
     def imaginary(self) -> float:
         return self.__im
-    
+
     def conjugate(self):
         return Complex(self.real(), -self.imaginary())
-        
+
     def reciprocal(self):
         if self.is_zero():
             raise ValueError("Complex is zero")
-            
+
         sum_of_squares = self.real() ** 2 + self.imaginary() ** 2
-        
+
         return Complex(self.real() / sum_of_squares, -self.imaginary() / sum_of_squares)
-            
+
     def duplicate(self):
         return Complex(self.real(), self.imaginary())
 
     def is_zero(self) -> bool:
         return math.isclose(self.real() ** 2 + self.imaginary() ** 2, 0)
-            
+
     def is_real(self) -> bool:
         return math.isclose(self.imaginary(), 0)
-        
+
     def is_pure(self) -> bool:
         return math.isclose(self.real(), 0)
 
--- algebra/TestComplex.py
+++ algebra/TestComplex.py
@@ -29,7 +29,7 @@ class TestComplex(unittest.TestCase):
         self.assertEqual(self.c - self.b, Complex(1, 7))
         self.assertEqual(self.d - self.c, Complex(3, 2))
         self.assertEqual(self.e - self.d, Complex(-10, -5))
-    
+
     def test_scalar_multiplication(self):
         self.assertEqual(self.a * 1, self.a)
         self.assertEqual(1 * self.a, self.a)
@@ -37,33 +37,33 @@ class TestComplex(unittest.TestCase):
         self.assertEqual(2 * self.b, Complex(0, -10))
         self.assertEqual(self.c * 3, Complex(3, 6))
         self.assertEqual(3 * self.c, Complex(3, 6))
-        
+
     def test_complex_multiplication(self):
         self.assertEqual(self.a @ self.b, Complex(5, -50))
         self.assertEqual(self.b @ self.c, Complex(10, -5))
         self.assertEqual(self.c @ self.d, Complex(-4, 12))
         self.assertEqual(self.d @ self.e, Complex(-20, -28))
         self.assertEqual(self.e @ self.a, Complex(-59, -16))
-        
+
     def test_equality(self):
         self.assertEqual(self.a, Complex(10, 1))
         self.assertEqual(self.b, Complex(0, -5))
         self.assertEqual(self.c, Complex(1, 2))
         self.assertEqual(self.d, Complex(4, 4))
         self.assertEqual(self.e, Complex(-6, -1))
-        
+
         self.assertEqual(self.a, self.a)
         self.assertEqual(self.b, self.b)
         self.assertEqual(self.c, self.c)
         self.assertEqual(self.d, self.d)
         self.assertEqual(self.e, self.e)
-        
+
         self.assertNotEqual(self.a, self.b)
         self.assertNotEqual(self.b, self.c)
         self.assertNotEqual(self.c, self.d)
         self.assertNotEqual(self.d, self.e)
         self.assertNotEqual(self.e, self.a)
-        
+
     def test_real_and_imaginary(self):
         self.assertEqual(self.a.real(), 10)
         self.assertEqual(self.a.imaginary(), 1)
@@ -75,14 +75,14 @@ class TestComplex(unittest.TestCase):
         self.assertEqual(self.d.imaginary(), 4)
         self.assertEqual(self.e.real(), -6)
         self.assertEqual(self.e.imaginary(), -1)
-        
+
     def test_conjugate(self):
         self.assertEqual(self.a.conjugate(), Complex(10, -1))
         self.assertEqual(self.b.conjugate(), Complex(0, 5))
         self.assertEqual(self.c.conjugate(), Complex(1, -2))
         self.assertEqual(self.d.conjugate(), Complex(4, -4))
         self.assertEqual(self.e.conjugate(), Complex(-6, 1))
-        
+
     def test_reciprocal(self):
         self.assertEqual(self.a.reciprocal(), Complex(0.099009900990099, -0.0099009900990099))
         self.assertEqual(self.b.reciprocal(), Complex(0, 0.2))
@@ -90,7 +90,7 @@ class TestComplex(unittest.TestCase):
         self.assertEqual(self.d.reciprocal(), Complex(0.125, -0.125))
         self.assertEqual(self.e.reciprocal(), Complex(-0.16216216216216, 0.027027027027027))
         self.assertEqual(self.f.reciprocal(), Complex(0.066666666666667, 0))
-    
+
     def test_duplicate(self):
         self.assertEqual(self.a.duplicate(), self.a)
         self.assertEqual(self.b.duplicate(), self.b)
@@ -98,14 +98,14 @@ class TestComplex(unittest.TestCase):
         self.assertEqual(self.d.duplicate(), self.d)
         self.assertEqual(self.e.duplicate(), self.e)
         self.assertEqual(self.f.duplicate(), self.f)
-        
+
         self.assertEqual(self.a.duplicate(), Complex(10, 1))
         self.assertEqual(self.b.duplicate(), Complex(0, -5))
         self.assertEqual(self.c.duplicate(), Complex(1, 2))
         self.assertEqual(self.d.duplicate(), Complex(4, 4))
         self.assertEqual(self.e.duplicate(), Complex(-6, -1))
         self.assertEqual(self.f.duplicate(), Complex(15, 0))
-        
+
     def test_is_zero(self):
         self.assertEqual(self.a.is_zero(), False)
         self.assertEqual(self.b.is_zero(), False)
@@ -113,7 +113,7 @@ class TestComplex(unittest.TestCase):
         self.assertEqual(self.d.is_zero(), False)
         self.assertEqual(self.e.is_zero(), False)
         self.assertEqual(Complex(0, 0).is_zero(), True)
-        
+
     def test_is_real(self):
         self.assertEqual(self.a.is_real(), False)
         self.assertEqual(self.b.is_real(), False)
@@ -121,7 +121,7 @@ class TestComplex(unittest.TestCase):
         self.assertEqual(self.d.is_real(), False)
         self.assertEqual(self.e.is_real(), False)
         self.assertEqual(self.f.is_real(), True)
-        
+
     def test_is_pure(self):
         self.assertEqual(self.a.is_pure(), False)
         self.assertEqual(self.b.is_pure(), True)
