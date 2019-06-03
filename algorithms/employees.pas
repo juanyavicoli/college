57df7f623da6baaba6d8c931e50351c51a8e91f0
adrianbielsa1 <50265158+adrianbielsa1@users.noreply.github.com>
2019-06-03 13:45:35 -0300

    Added problem description

---
+++ algorithms/employees.pas
@@ -0,0 +1,24 @@
+// PROBLEM: Read information of 1000 employees, using
+// a repeat-until loop.
+
+// Information that needs to be gathered:
+
+// File number [integer]
+// First and last name [string20]
+// Category (A, B or C) [char]
+// Sex (H or M) [char]
+// Sector of work (Prod or Admi) [string or char]
+// Age [integer]
+// Salary [real]
+
+// Information that needs to be displayed:
+
+// Amount of workers per category
+// Average men salary per category
+// Average women salary per category
+// First and last name of the employee with the highest
+// file number per category
+// Average men age working at "Prod" category
+// Number of times the highest age is repeated
+// Category with the highest amount of workers
+
