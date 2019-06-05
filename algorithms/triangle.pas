c7a53a075b23b7eea954f12d771b5b09eef22d44
adrianbielsa1 <50265158+adrianbielsa1@users.noreply.github.com>
2019-06-05 00:41:00 -0300

    Added the complete solution for the given problem

--- algorithms/triangle.pas
+++ algorithms/triangle.pas
@@ -1,3 +1,52 @@
 // PROBLEM: Write a program that displays a triangle
 // to the console using a procedure.
 
+program proc;
+
+// This procedure was proposed by our professor. It
+// simply illustrates how to define and use a procedure.
+procedure fixed_triangle;
+    begin
+        
+        WriteLn('    *    ');
+        WriteLn('  *   *  ');
+        WriteLn(' *     * ');
+        WriteLn('*       *');
+        
+    end;
+    
+procedure triangle(size: Integer);
+    var
+        x: Integer;
+        c: Integer;
+        
+    begin
+    
+        // This line is really important, otherwise, a garbage
+        // value from the previous call will be taken.
+        c := 0;
+        
+        while c < size do
+            begin
+            
+                for x := 1 to size - c do
+                    Write(' ');
+                    
+                for x := size - c to size + c do
+                    Write('*');
+                    
+                WriteLn('');
+                c := c + 1;
+            
+            end;
+        
+    end;
+
+begin
+
+    fixed_triangle();
+    triangle(3);
+    triangle(4);
+
+end.
+
