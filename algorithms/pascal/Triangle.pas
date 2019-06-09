655536b66186f220393f95a9b5d3f0e34c40d2d9
adrianbielsa1 <50265158+adrianbielsa1@users.noreply.github.com>
2019-06-09 14:54:16 -0300

    Removed dangling trailing spaces

--- algorithms/pascal/Triangle.pas
+++ algorithms/pascal/Triangle.pas
@@ -7,39 +7,39 @@ program Triangle;
 // simply illustrates how to define and use a procedure.
 procedure fixed_triangle;
     begin
-        
+
         WriteLn('    *    ');
         WriteLn('  *   *  ');
         WriteLn(' *     * ');
         WriteLn('*       *');
-        
+
     end;
-    
 procedure triangle(size: Integer);
+
     var
         x: Integer;
         c: Integer;
-        
+
     begin
-    
+
         // This line is really important, otherwise, a garbage
         // value from the previous call will be taken.
         c := 0;
-        
+
         while c < size do
             begin
-            
+
                 for x := 1 to size - c do
                     Write(' ');
-                    
+
                 for x := size - c to size + c do
                     Write('*');
-                    
+
                 WriteLn('');
                 c := c + 1;
-            
+
             end;
-        
+
     end;
 
 begin
