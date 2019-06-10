6bc65a07fd936c293998d309aabd23ecb7eda3bf
adrianbielsa1 <50265158+adrianbielsa1@users.noreply.github.com>
2019-06-10 16:57:20 -0300

    Added and defined function recursive_fibonacci() and its tests

---
+++ algorithms/rust/Fibonacci.rs
@@ -0,0 +1,23 @@
+fn recursive_fibonacci(n: u64) -> u64 {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2)
+    }
+}
+
+#[test]
+fn test_recursive_fibonacci() {
+    assert_eq!(recursive_fibonacci(0), 0);
+    assert_eq!(recursive_fibonacci(1), 1);
+    assert_eq!(recursive_fibonacci(2), 1);
+    assert_eq!(recursive_fibonacci(3), 2);
+    assert_eq!(recursive_fibonacci(4), 3);
+    assert_eq!(recursive_fibonacci(5), 5);
+    assert_eq!(recursive_fibonacci(6), 8);
+    assert_eq!(recursive_fibonacci(7), 13);
+    assert_eq!(recursive_fibonacci(8), 21);
+}
+
