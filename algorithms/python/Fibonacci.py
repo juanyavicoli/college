d756032e3944710e31bedb7d7cda5894fee166df
adrianbielsa1 <50265158+adrianbielsa1@users.noreply.github.com>
2019-06-11 17:58:27 -0300

    Simplified function recursive_fibonacci() and modified the error message

--- algorithms/python/Fibonacci.py
+++ algorithms/python/Fibonacci.py
@@ -1,11 +1,8 @@
 def recursive_fibonacci(n: int) -> int:
     if n < 0:
-        raise ValueError("N must be positive integer.")
-
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
+        raise ValueError("n argument must non-negative.")
+    elif n < 2:
+        return n
     else:
         return recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2)
 
