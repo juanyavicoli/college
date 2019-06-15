87fef671b8fd562c7e60788ef2e598adf64495cc
adrianbielsa1 <50265158+adrianbielsa1@users.noreply.github.com>
2019-06-15 00:31:30 -0300

    Added and defined functions while_loop_factorial() and for_loop_factorial() and used them to print some sample values

--- algorithms/pascal/Factorial.pas
+++ algorithms/pascal/Factorial.pas
@@ -1,5 +1,6 @@
 program Factorial;
 
+
 function recursive_factorial(n: integer): integer;
 begin
     // For simplicity, and since we weren't teached
@@ -11,6 +12,33 @@ begin
         recursive_factorial := n * recursive_factorial(n - 1);
 end;
 
+
+function while_loop_factorial(n: integer): integer;
+var
+    c: integer;
+begin
+    while_loop_factorial := 1;
+    c := 1;
+
+    while c < n do
+    begin
+        while_loop_factorial := while_loop_factorial * c;
+        c := c + 1;
+    end;
+end;
+
+
+function for_loop_factorial(n: integer): integer;
+var
+    c: integer;
+begin
+    for_loop_factorial := 1;
+
+    for c := 1 to n do
+        for_loop_factorial := for_loop_factorial * c;
+end;
+
+
 begin
 
     WriteLn(recursive_factorial(0));
@@ -20,6 +48,22 @@ begin
     WriteLn(recursive_factorial(4));
     WriteLn(recursive_factorial(5));
     WriteLn(recursive_factorial(6));
+
+    WriteLn(while_loop_factorial(0));
+    WriteLn(while_loop_factorial(1));
+    WriteLn(while_loop_factorial(2));
+    WriteLn(while_loop_factorial(3));
+    WriteLn(while_loop_factorial(4));
+    WriteLn(while_loop_factorial(5));
+    WriteLn(while_loop_factorial(6));
+
+    WriteLn(for_loop_factorial(0));
+    WriteLn(for_loop_factorial(1));
+    WriteLn(for_loop_factorial(2));
+    WriteLn(for_loop_factorial(3));
+    WriteLn(for_loop_factorial(4));
+    WriteLn(for_loop_factorial(5));
+    WriteLn(for_loop_factorial(6));
 
 end.
 
